12c037cad742b7bc94b84a98eca02c57293c001a
samuelsenerwa <samuelsenerwa@gmail.com>
2023-12-28 14:08:08 +0300

    feat: added modal and button component and fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 export const metadata: Metadata = {
   title: "Airbnb",
@@ -20,7 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Modal title="Hello world" actionLabel="Submit" isOpen />
+          <Navbar />
+        </ClientOnly>
+
         {children}
       </body>
     </html>
