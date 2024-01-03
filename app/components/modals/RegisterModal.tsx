b1abbaea89b8782cdd97590a429a794ca0dd3f09
samuelsenerwa <samuelsenerwa@gmail.com>
2024-01-03 11:09:54 +0300

    feat: created Heading component to be used to the pop up for registration

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+import useRegisterModal from "@/app/hooks/useRegisterModel";
+import { error } from "console";
+import Modal from "./Modal";
+import Heading from "../Heading";
+
+// establish form control with the function
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      defaultValues: {
+        name: "",
+        email: "",
+        password: "",
+      },
+    },
+  });
+
+  // submit handler
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+//   The  modal accepts more content that is the header and the footer
+
+const bodyContent = (
+    <div className="flex flex-col gap-4">
+        <Heading title={"Welcome to Airbnb"} subtitle={"Create an Account"}/>
+    </div>
+)
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+    />
+  );
+};
+
+export default RegisterModal;
