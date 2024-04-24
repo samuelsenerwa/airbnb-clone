48687ca87de265ee30fe33aa12761e839ae569f8
samuelsenerwa <samuelsenerwa@gmail.com>
2024-04-24 12:11:11 +0300

    feat:  added location listing with map functionality && corrected hook file naming

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -7,6 +7,8 @@ import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
 
 enum STEPS {
   CATEGORY = 0,
@@ -46,6 +48,16 @@ const RentModal = () => {
   });
 
   const category = watch("category");
+  const location = watch("location");
+
+  //   dynamically render map import
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../Map"), {
+        ssr: false,
+      }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -103,11 +115,27 @@ const RentModal = () => {
     return "Back";
   }, [step]);
 
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-4">
+        <Heading
+          title="where is your place located?"
+          subtitle="Help guests find you?"
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue("location", location)}
+        />
+        <Map center={location?.latlng} />
+      </div>
+    );
+  }
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
