aa54618775696ab0a8851a79a010b15f2a0f10c0
samuelsenerwa <samuelsenerwa@gmail.com>
2024-04-25 19:16:52 +0300

    feat: added fetching listings data && minor fixes

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -131,7 +131,7 @@ const RentModal = () => {
         subtitle="Pick a category"
         center={true}
       />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h[50vh] overflow-y-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
         {categories.map((item) => (
           <div key={item.label} className="col-span-1">
             <CategoryInput
@@ -166,7 +166,7 @@ const RentModal = () => {
         />
         <CountrySelect
           value={location}
-          onChange={(value) => setCustomValue("location", location)}
+          onChange={(value) => setCustomValue("location", value)}
         />
         <Map center={location?.latlng} />
       </div>
